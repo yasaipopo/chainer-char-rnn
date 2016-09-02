a4bda45945506f29555f0ca857c7391b517956b9
yasaipopo <yasaipopo@gmail.com>
2016-09-02 20:43:12 +0900

    mecab

--- sample.py
+++ sample.py
@@ -1,36 +1,73 @@
-#%%
+# coding:utf-8
 import time
 import math
 import sys
 import argparse
 import cPickle as pickle
 import codecs
+import os.path
 
 import numpy as np
 from chainer import cuda, Variable, FunctionSet
 import chainer.functions as F
 from CharRNN import CharRNN, make_initial_state
+import MeCab
+import re
+mecab = MeCab.Tagger ("-Ochasen")
+
+def add_text(state):
+    node = mecab.parseToNode(args.primetext)
+    while node:
+        if node.surface=="":
+            node=node.next
+            continue
+        i=node.surface+"::"+node.feature
+        sys.stdout.write(i.decode("utf-8").split("::")[0])
+        prev_char = np.ones((1,), dtype=np.int32) * vocab[i]
+        if args.gpu >= 0:
+            prev_char = cuda.to_gpu(prev_char)
+
+        state, prob = model.forward_one_step(prev_char, prev_char, state, train=False)
+        node = node.next
+    return state,prob,prev_char
 
 sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
 
 #%% arguments
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--model',      type=str,   required=True)
-parser.add_argument('--vocabulary', type=str,   required=True)
+parser.add_argument('--model',      type=str,   default='cv/latest.chainermodel')
+parser.add_argument('--vocabulary', type=str,   default='')
 
 parser.add_argument('--seed',       type=int,   default=123)
 parser.add_argument('--sample',     type=int,   default=1)
 parser.add_argument('--primetext',  type=str,   default='')
 parser.add_argument('--length',     type=int,   default=2000)
-parser.add_argument('--gpu',        type=int,   default=-1)
+parser.add_argument('--gpu',        type=int,   default=0)
+parser.add_argument('--delimiter',  type=str,   default="-=-=-=-=-=-=-=-")
 
 args = parser.parse_args()
+#delimiter
+delimiter_a = []
+if args.delimiter!="":
+    node = mecab.parseToNode(args.delimiter)
+    while node:
+        delimiter_a.append(node.surface+"::"+"_".join(node.feature.split(",")[0:2]))
+        node=node.next
+
 
 np.random.seed(args.seed)
 
+if os.path.isdir(args.model):
+    args.model += "/latest.chainermodel"
+
 # load vocabulary
-vocab = pickle.load(open(args.vocabulary, 'rb'))
+
+if args.vocabulary!='':
+    vocab = pickle.load(open(args.vocabulary, 'rb'))
+else:
+    vocab = pickle.load(open(os.path.dirname(args.model)+"/vocab.bin", 'rb'))
+
 ivocab = {}
 for c, i in vocab.items():
     ivocab[i] = c
@@ -54,14 +91,11 @@ if args.gpu >= 0:
     prev_char = cuda.to_gpu(prev_char)
 
 if len(args.primetext) > 0:
-    for i in unicode(args.primetext, 'utf-8'):
-        sys.stdout.write(i)
-        prev_char = np.ones((1,), dtype=np.int32) * vocab[i]
-        if args.gpu >= 0:
-            prev_char = cuda.to_gpu(prev_char)
+    state,prob,prev_char = add_text(state)
 
-        state, prob = model.forward_one_step(prev_char, prev_char, state, train=False)
 
+
+delimiter_hit_count=0
 for i in xrange(args.length):
     state, prob = model.forward_one_step(prev_char, prev_char, state, train=False)
 
@@ -71,10 +105,25 @@ for i in xrange(args.length):
         index = np.random.choice(range(len(probability)), p=probability)
     else:
         index = np.argmax(cuda.to_cpu(prob.data))
-    sys.stdout.write(ivocab[index])
-
+    sys.stdout.write(ivocab[index].decode("utf-8").split("::")[0])
     prev_char = np.array([index], dtype=np.int32)
     if args.gpu >= 0:
         prev_char = cuda.to_gpu(prev_char)
 
+
+    if len(delimiter_a)>0 and len(args.primetext) > 0:
+        if ivocab[index]==delimiter_a[delimiter_hit_count]:
+            delimiter_hit_count+=1
+            if delimiter_hit_count==len(delimiter_a):
+                state,prob,prev_char=add_text(state)
+                delimiter_hit_count=0
+        else:
+            delimiter_hit_count=0
+
+    #print ivocab[index].decode("utf-8")
+
+
+
 print
+
+
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-#%%
+# coding:utf-8
 import time
 import math
 import sys
@@ -12,13 +12,22 @@ import numpy as np
 from chainer import cuda, Variable, FunctionSet, optimizers
 import chainer.functions as F
 from CharRNN import CharRNN, make_initial_state
+import MeCab
 
 # input data
 def load_data(args):
     vocab = {}
     print ('%s/input.txt'% args.data_dir)
-    words = codecs.open('%s/input.txt' % args.data_dir, 'rb', 'utf-8').read()
-    words = list(words)
+    line_a = codecs.open('%s/input.txt' % args.data_dir, 'rb', 'utf-8')
+    words = []
+    m = MeCab.Tagger ("-Ochasen")
+    for line in line_a:
+	node = m.parseToNode(line.encode("utf-8"))
+	while node:
+	    #print "_".join(node.feature.split(",")[0:2])
+	    words.append(node.surface+"::"+node.feature)
+	    node = node.next
+        words.append("\n")
     dataset = np.ndarray((len(words),), dtype=np.int32)
     for i, word in enumerate(words):
         if word not in vocab:
@@ -30,9 +39,9 @@ def load_data(args):
 
 # arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir',                   type=str,   default='data/tinyshakespeare')
-parser.add_argument('--checkpoint_dir',             type=str,   default='cv')
-parser.add_argument('--gpu',                        type=int,   default=-1)
+parser.add_argument('--data_dir',                   type=str,   default='data/tw')
+parser.add_argument('--checkpoint_dir',             type=str,   default='')
+parser.add_argument('--gpu',                        type=int,   default=0)
 parser.add_argument('--rnn_size',                   type=int,   default=128)
 parser.add_argument('--learning_rate',              type=float, default=2e-3)
 parser.add_argument('--learning_rate_decay',        type=float, default=0.97)
@@ -47,19 +56,30 @@ parser.add_argument('--init_from',                  type=str,   default='')
 
 args = parser.parse_args()
 
-if not os.path.exists(args.checkpoint_dir):
-    os.mkdir(args.checkpoint_dir)
 
+c_dir = 'r'+str(args.rnn_size)+'_s'+str(args.seq_length)
+if args.checkpoint_dir!='':
+    c_dir = args.checkpoint_dir
+
+if not os.path.exists(c_dir):
+    os.mkdir(c_dir)
+
+#利用した学習データに対して更新を終えるのが、1エポックというサイクル
 n_epochs    = args.epochs
 n_units     = args.rnn_size
+# minibatch size
 batchsize   = args.batchsize
+#BPTTしたときtruncateする時間サイズを指定。
 bprop_len   = args.seq_length
+#この関数は勾配のL2normの大きさがgrad_clipよりも大きい場合、この大きさに縮める処理を行うようです。
 grad_clip   = args.grad_clip
 
 train_data, words, vocab = load_data(args)
 pickle.dump(vocab, open('%s/vocab.bin'%args.data_dir, 'wb'))
+pickle.dump(vocab, open('%s/vocab.bin'%c_dir, 'wb'))
 
 if len(args.init_from) > 0:
+    print args.init_from
     model = pickle.load(open(args.init_from, 'rb'))
 else:
     model = CharRNN(len(vocab), n_units)
@@ -114,10 +134,12 @@ for i in xrange(jump * n_epochs):
         optimizer.clip_grads(grad_clip)
         optimizer.update()
 
-    if (i + 1) % 10000 == 0:
-        fn = ('%s/charrnn_epoch_%.2f.chainermodel' % (args.checkpoint_dir, float(i)/jump))
-        pickle.dump(copy.deepcopy(model).to_cpu(), open(fn, 'wb'))
-        pickle.dump(copy.deepcopy(model).to_cpu(), open('%s/latest.chainermodel'%(args.checkpoint_dir), 'wb'))
+    if (i + 1) % 1000 == 0:
+        fn = ('%s/charrnn_epoch_%.2f.chainermodel' % (c_dir, float(i)/jump))
+        if((i + 1)/bprop_len) % 5000== 0:
+            pickle.dump(copy.deepcopy(model).to_cpu(), open(fn, 'wb'))
+        #pickle.dump(copy.deepcopy(model).to_cpu(), open(fn, 'wb'))
+        pickle.dump(copy.deepcopy(model).to_cpu(), open('%s/latest.chainermodel'%(c_dir), 'wb'))
 
     if (i + 1) % jump == 0:
         epoch += 1
